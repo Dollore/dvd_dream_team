3a55e9e6a7acbe78a1e1ef28bd3b737a134d4bf0
zuza3D <64960290+zuza3D@users.noreply.github.com>
2020-05-13 00:13:57 +0200

    adding user_profile and updating styles.css

--- backtopast/resources/views/user_profile.blade.php
+++ backtopast/resources/views/user_profile.blade.php
@@ -3,6 +3,37 @@
 
 @section('content')
 
-    <h1>Tu robicie Wasz content </h1>
+<h2 class="myWhiteCenter">Ogólne ustawienia konta</h2> 
+    <div id="myChangeSettings">
+        <div class="container">
+            <table>
+                <tr>
+                    <td>Imię i nazwisko:</td>
+                    <td>Hieronim Dupogrzybek</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Nazwa użytkownika:</td>
+                    <td>hieronimous</td>
+                    <td><button class="myButtonOnlyText btn btn-secondary">noproblem</button></td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td>hieronim@gmail.com</td>
+                    <td><button class="myButtonOnlyText btn btn-secondary">noproblem</button></td>
+                </tr>
+                <tr>
+                    <td>Hasło:</td>
+                    <td>******</td>
+                    <td><button class="myButtonOnlyText btn btn-secondary">noproblem</button></td>
+                </tr>
+                <tr>
+                    <td>Telefon:</td>
+                    <td>666123561</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
 
 @endsection
