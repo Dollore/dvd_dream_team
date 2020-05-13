deac7f739c73eae8638c8d55191aa5336d9d5629
Dollore <64959401+Dollore@users.noreply.github.com>
2020-05-13 12:23:55 +0200

    Film_page update

--- backtopast/resources/views/film_page.blade.php
+++ backtopast/resources/views/film_page.blade.php
@@ -3,6 +3,73 @@
 
 @section('content')
 
-    <h1>Tu robicie Wasz content </h1>
+    <div class="row justify-content-md-center">
+
+            <div class="col">
+                <h1>Tytuł filmu</h1>
+            </div>
+
+            <div class="col-md-auto">
+                
+            </div>
+            
+            <div class="col button_film" >
+                <button type="button" class="btn btn-primary" >Zamów</button>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col karuzela justify-content-md-center">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="img\the_shawshank_redemption\tytul.jpg" class="d-block img-fluid" alt="Karta tytułowa">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img\the_shawshank_redemption\cast.jpg" class="d-block img-fluid" alt="cast">
+                        </div>
+
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Poprzednie</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Następne</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="row">
+                <div class="col">
+                    <h1>Tytuł filmu</h1>
+                </div>
+            </br>
+                <div>
+                    <div class="col">
+                        <span>Gatunek</span>
+                    </div>
+                    <br>
+                    <div class="col">
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aspernatur ipsam, dolor culpa tempora consectetur. Adipisci quidem, eos beatae nisi repellat quibusdam, sunt doloribus illum voluptatibus distinctio, nesciunt error nam!</span>
+                    </div>
+                    <br>
+                    <div class="row ">
+                        <div class="col-sm col-6 col-sm-4">
+                            <span>rezyser</span>
+                        </div>
+                        <div class="col-sm col-6 col-sm-4">
+                            aktorzy
+                        </div>
+                    </div>
+                </div>
+        </div>
 
 @endsection
