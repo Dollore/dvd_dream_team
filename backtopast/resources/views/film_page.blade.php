040152beccc38a87cc81f23a445e2f331312a40a
rafalstarypan <64851904+rafalstarypan@users.noreply.github.com>
2020-05-18 22:16:06 +0200

    Film carousel 1

--- backtopast/resources/views/film_page.blade.php
+++ backtopast/resources/views/film_page.blade.php
@@ -3,72 +3,93 @@
 
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/film_page_styles.css') }}" >
-<div class="container">
-    <div class="row d-flex justify-content-between">
+<link href="{{ asset('js/package/css/swiper.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/film_carousel_styles.css') }}" rel="stylesheet">
 
-            <div class="myTitle" >
-                <h1>Tytuł filmu</h1>
-            </div>
+</div> <!-- Zamykam myFlex z layouta -->
+<!--<div class="row d-flex justify-content-between"> -->
 
-            <div class="col-md-auto">
-                
-            </div>
-            
-
-            
-        </div>
-        <div class="row">
-            <div class="col-12 karuzela">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="{{ asset('img/films/zielona_mila/1.jpeg') }}" class="d-block img-fluid" alt="Karta tytułowa">
-                        </div>
-                        
-                        <div class="carousel-item">
-                            <img src="{{ asset('img/films/zielona_mila/2.jpg') }}" class="d-block img-fluid" alt="cast">
-                        </div>
-
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Poprzednie</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Następne</span>
-                    </a>
-                </div>
-            </div>
+        <div class="myTitle" >
+            <h1>Tytuł filmu</h1>
+        </div> 
+<!--</div> -->
+    <!--<div class="row"> -->
         
-            <div class="col-12 button_film " >
-            <button type="button" class="btn mySubmitButton btn-lg myBorrowButton" >Wypożycz</button>
+    <div class="netflix-slider ">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><img src="{{ asset('img/films/zielona_mila/1.jpeg') }}" alt="Movie Title"></div>
+                <div class="swiper-slide"><img src="{{ asset('img/films/zielona_mila/2.jpg') }}" alt="Movie Title"></div>
+                <div class="swiper-slide"><img src="{{ asset('img/films/zielona_mila/3.jpeg') }}" alt="Movie Title"></div>
+                <div class="swiper-slide"><img src="{{ asset('img/films/zielona_mila/4.jpg') }}" alt="Movie Title"></div>
             </div>
-            
-            <div class="row">
-                <div class="col-12">
-                    <h2>Tytuł filmu</h2>
-                </div>
-            </br>
-                <div>
-                    <div class="col-12">
-                        <span>Gatunek</span>
-                    </div>
-                    <br>
-                    <div class="col-12">
-                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aspernatur ipsam, dolor culpa tempora consectetur. Adipisci quidem, eos beatae nisi repellat quibusdam, sunt doloribus illum voluptatibus distinctio, nesciunt error nam! lorem10000</span>
-                    </div>
-                    <br>
-                    <div class="row ">
-                        <div class="col-sm-12 col-md-6">
-                            <span>rezyser</span>
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            aktorzy
-                        </div>
-                    </div>
-                </div>
+
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
-</div>
+    </div>
+    
+        <div class="col-12 button_film " >
+        <button type="button" class="btn mySubmitButton btn-lg myBorrowButton" >Wypożycz</button>
+        </div>
+        
+        <div class="row">
+            <div class="col-12">
+                <h2>Tytuł filmu</h2>
+            </div>
+
+            <div>
+                <div class="col-12">
+                    <span>Gatunek</span>
+                </div>
+                <br>
+                <div class="col-12">
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aspernatur ipsam, dolor culpa tempora consectetur. Adipisci quidem, eos beatae nisi repellat quibusdam, sunt doloribus illum voluptatibus distinctio, nesciunt error nam! lorem10000</span>
+                </div>
+                <br>
+                <div class="row ">
+                    <div class="col-sm-12 col-md-6">
+                        <span>rezyser</span>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        aktorzy
+                    </div>
+                </div>
+            </div>
+    <!--</div> -->
+
+<!-- Swiper JS -->
+<script src="{{ asset('js/package/js/swiper.js') }}"> </script>
+
+<!-- Initialize Swiper -->
+<script>
+  var swiper = new Swiper('.swiper-container', {
+    
+    init: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    slidesPerGroupSkip: 1,
+    updateOnWindowResize: true,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    setWrapperSize: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+
+  });
+
+</script>
+
+<div class="py-4 myFlex"> <!-- Żeby się nie gryzl z zamknieciem myFlex z layouta -->
+
 @endsection
